b12b936e5ef2bb5a69c94360b0d18d023404a845
holystorySeo <holystory.seo@gmail.com>
2022-04-04 17:53:04 +0900

    [Feat]복잡한 사칙연산 기능

--- script.js
+++ script.js
@@ -1,14 +1,51 @@
 const displayResult = document.querySelector(".calculator-display");
 const buttons = document.querySelector(".calculator-button");
-let cnt = 0;
+let cnt = 0; // 입력된 숫자의 length가 25이상이 되었을 때 부터 계산
+let firstNum, operator, previousNum, previousKey;
+
+function calculator(num1, operator, num2) {
+  let result = 0;
+
+  if (operator === "+") {
+    result = num1 + num2;
+  }
+
+  if (operator === "−") {
+    result = num1 - num2;
+  }
+
+  if (operator === "×") {
+    result = num1 * num2;
+  }
+
+  if (operator === "÷") {
+    result = num1 / num2;
+  }
+
+  return result;
+}
 
 buttons.addEventListener("click", (e) => {
-  if (e.target.className == "number") {
-    if (displayResult.textContent === "0") {
-      displayResult.textContent = e.target.textContent;
+  const target = e.target;
+  const name = target.className;
+  const buttonContent = target.textContent;
+
+  if (name == "number") {
+    // 숫자 입력의 경우
+    if (
+      displayResult.textContent === "0" ||
+      (operator !== undefined &&
+        firstNum !== undefined &&
+        previousNum === undefined) ||
+      previousKey !== undefined
+    ) {
+      console.log("1");
+      displayResult.textContent = buttonContent;
+      previousKey = undefined;
+      if (operator !== undefined) {
+        previousNum = buttonContent;
+      }
     } else {
-      console.log(displayResult.textContent.length);
-      console.log(cnt);
       if (displayResult.textContent.length >= 10) {
         displayResult.style.fontSize = "2.5rem";
       }
@@ -32,8 +69,8 @@ buttons.addEventListener("click", (e) => {
         displayResult.textContent = displayResult.textContent.slice(1) + 0;
         cnt += 1;
       } else {
-        displayResult.textContent =
-          displayResult.textContent + e.target.textContent;
+        console.log("2");
+        displayResult.textContent = displayResult.textContent + buttonContent;
       }
 
       if (cnt === 26) {
@@ -41,5 +78,37 @@ buttons.addEventListener("click", (e) => {
         displayResult.style.fontSize = "3rem";
       }
     }
+  }
+
+  if (name === "operator") {
+    if (firstNum === undefined) {
+      firstNum = displayResult.textContent;
+      operator = buttonContent;
+    } else {
+      displayResult.textContent = calculator(
+        Number(firstNum),
+        operator,
+        Number(displayResult.textContent)
+      );
+      firstNum = displayResult.textContent;
+      operator = buttonContent;
+      previousNum = undefined;
+      previousKey = "=";
+    }
+  }
+
+  if (name === "decimal") {
+  }
+
+  if (name === "calculate") {
+    displayResult.textContent = calculator(
+      Number(firstNum),
+      operator,
+      Number(displayResult.textContent)
+    );
+    firstNum = undefined;
+    operator = undefined;
+    previousNum = undefined;
+    previousKey = "=";
   }
 });
