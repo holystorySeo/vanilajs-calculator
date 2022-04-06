200e37d52243f10f85fd644a83123432a6e2ae96
holystorySeo <holystory.seo@gmail.com>
2022-04-06 17:30:06 +0900

    [Feat]bare minimum 테스트 코드 통과

--- spec/script.test.js
+++ spec/script.test.js
@@ -5,9 +5,9 @@ if (typeof window === "undefined") {
   const cwd = process.cwd();
   const { JSDOM } = require("jsdom");
   const { expect } = require("chai");
-  require("mocha");
+  console.log(`경로, ${cwd}`);
 
-  const myLibrary = fs.readFileSync(path.join(cwd, "/script.js"), {
+  const prepared = fs.readFileSync(path.join(cwd, "/script.js"), {
     encoding: "utf-8"
   });
   const html = fs.readFileSync(path.join(cwd, "/calculator.html"));
@@ -15,7 +15,7 @@ if (typeof window === "undefined") {
   let window;
   window = new JSDOM(html, { runScripts: "dangerously" }).window;
   const script = window.document.createElement("script");
-  script.textContent = myLibrary;
+  script.textContent = prepared;
 
   window.document.body.appendChild(script);
 
@@ -73,17 +73,227 @@ function bare(window, expect) {
       ...operatorButtons
     ];
 
-    it("처음 숫자 버튼을 눌렀을 때, 첫 번째 화면에 누른 숫자가 보여야 합니다.", function (done) {
-      const test = ["7", "7"];
-      const clicks = test.slice(0, -1);
-      const expected = test.slice(-1)[0];
-      const display = window.document.querySelector(".calculator-display");
-      clicks.forEach(function (click) {
-        const button = getButtonBy(click, allButtons);
-        button.dispatchEvent(clickEvent);
+    describe("Step 1 - 숫자 버튼을 누르고 화면에 숫자를 입력하기", function () {
+      it("처음 숫자 버튼을 눌렀을 때, 첫 번째 화면에 누른 숫자가 보여야 합니다.", function (done) {
+        const test = ["3", "3"];
+        const clicks = test.slice(0, -1);
+        const expected = test.slice(-1)[0];
+        const display = window.document.querySelector(".calculator-display");
+        clicks.forEach(function (click) {
+          const button = getButtonBy(click, allButtons);
+          button.dispatchEvent(clickEvent);
+        });
+        expect(display.textContent).to.equal(expected);
+        done();
       });
-      expect(display.textContent).to.equal(expected);
-      done();
+
+      it("숫자 버튼을 여러 번 눌렀을 때, 계산기 화면에 숫자가 이어붙여져야(concatenation) 합니다.", function (done) {
+        const test = ["7", "0", "0", "0", "7000"];
+        const clicks = test.slice(0, -1);
+        const expected = test.slice(-1)[0];
+        const display = window.document.querySelector(".calculator-display");
+        clicks.forEach(function (click) {
+          const button = getButtonBy(click, allButtons);
+          button.dispatchEvent(clickEvent);
+        });
+        expect(display.textContent).to.equal(expected);
+        done();
+      });
+    });
+
+    describe("Step 2 - '=' 버튼 눌러 계산하고, AC 버튼으로 초기화 시키기", function () {
+      it("연산자 버튼을 눌렀을 때, 계산기 화면에 보이는 숫자를 따로 저장하고 계산할 준비를 해야 합니다.", function (done) {
+        const test = ["3", "0", "0", "0", "×", "3000"];
+        const clicks = test.slice(0, -1); //["3", "0", "0", "0", "×"];
+        const expected = test.slice(-1)[0];
+        const display = window.document.querySelector(".calculator-display");
+        clicks.forEach(function (click) {
+          const button = getButtonBy(click, allButtons);
+          button.dispatchEvent(clickEvent);
+        });
+        expect(display.textContent).to.equal(expected);
+        done();
+      });
+
+      it("= 버튼을 눌렀을 때, 계산기 화면에 보이는 숫자와 저장된 숫자를 함께 조합하여 계산한 결과를 화면에 보여줘야 합니다.", function (done) {
+        const test = ["3", "0", "0", "0", "×", "3", "=", "9000"];
+        const clicks = test.slice(0, -1); //["3", "0", "0", "0", "×"];
+        const expected = test.slice(-1)[0];
+        const display = window.document.querySelector(".calculator-display");
+        clicks.forEach(function (click) {
+          const button = getButtonBy(click, allButtons);
+          button.dispatchEvent(clickEvent);
+        });
+        expect(display.textContent).to.equal(expected);
+        done();
+      });
+    });
+
+    describe("AC 버튼이 제대로 작동하는지 테스트 합니다.", function () {
+      afterEach(() => {
+        clearButton.dispatchEvent(clickEvent);
+      });
+
+      it("AC 버튼을 클릭하면 화면이 초기화 되어야 합니다.", function (done) {
+        const display = window.document.querySelector(".calculator-display");
+        display.textContent = "bug stuff";
+        const clearButton = window.document.querySelector(".clear");
+        clearButton.dispatchEvent(clickEvent);
+
+        expect(display.textContent).to.equal("0");
+        done();
+      });
+    });
+
+    describe("calculator 함수를 검사합니다.", () => {
+      describe("정수의 연산을 테스트 합니다", () => {
+        const calculateFunc = (testElement) => {
+          const { firstNum, operator, previousNum, result } = testElement;
+
+          it(`${firstNum}과 ${previousNum}의 계산 결과는 ${result}이여야 합니다.`, function (done) {
+            const stringInputResult = window.calculator(
+              firstNum,
+              operator,
+              previousNum
+            );
+
+            const numberInputResult = window.calculator(
+              firstNum,
+              operator,
+              previousNum
+            );
+
+            const isPassed =
+              Boolean(stringInputResult === Number(result)) ||
+              Boolean(numberInputResult === Number(result));
+            expect(isPassed).to.be.true;
+            done();
+          });
+        };
+
+        describe("덧샘 연산을 검사합니다", function () {
+          const testArr = [
+            {
+              firstNum: "1",
+              operator: "+",
+              previousNum: "2",
+              result: "3"
+            },
+            {
+              firstNum: "5689",
+              operator: "+",
+              previousNum: "1234",
+              result: "6923"
+            },
+            {
+              firstNum: "24",
+              operator: "+",
+              previousNum: "12",
+              result: "36"
+            },
+            {
+              firstNum: "1000",
+              operator: "+",
+              previousNum: "3100",
+              result: "4100"
+            }
+          ];
+
+          testArr.forEach(calculateFunc);
+        });
+
+        describe("뺄샘 연산을 검사합니다", function () {
+          const testArr = [
+            {
+              firstNum: "1",
+              operator: "−",
+              previousNum: "2",
+              result: "-1"
+            },
+            {
+              firstNum: "5784",
+              operator: "−",
+              previousNum: "5784",
+              result: "0"
+            },
+            {
+              firstNum: "1000",
+              operator: "−",
+              previousNum: "1100",
+              result: "-100"
+            },
+            {
+              firstNum: "1100",
+              operator: "−",
+              previousNum: "1000",
+              result: "100"
+            }
+          ];
+
+          testArr.forEach(calculateFunc);
+        });
+
+        describe("곱샘 연산을 검사합니다", function () {
+          const testArr = [
+            {
+              firstNum: "1",
+              operator: "×",
+              previousNum: "2",
+              result: "2"
+            },
+            {
+              firstNum: "5425",
+              operator: "×",
+              previousNum: "243",
+              result: "1318275"
+            },
+            {
+              firstNum: "1000",
+              operator: "×",
+              previousNum: "100",
+              result: "100000"
+            },
+            {
+              firstNum: "100",
+              operator: "×",
+              previousNum: "0",
+              result: "0"
+            }
+          ];
+
+          testArr.forEach(calculateFunc);
+        });
+
+        describe("나눗샘 연산을 검사합니다", function () {
+          const testArr = [
+            {
+              firstNum: "5",
+              operator: "÷",
+              previousNum: "2",
+              result: "2.5"
+            },
+            {
+              firstNum: "100",
+              operator: "÷",
+              previousNum: "10",
+              result: "10"
+            },
+            {
+              firstNum: "2048",
+              operator: "÷",
+              previousNum: "1024",
+              result: "2"
+            },
+            {
+              firstNum: "69249",
+              operator: "÷",
+              previousNum: "123",
+              result: "563"
+            }
+          ];
+          testArr.forEach(calculateFunc);
+        });
+      });
     });
   });
 }
